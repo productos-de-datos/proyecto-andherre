0ac22f0baed2d52fd843b747a27f64201fee9315
andherre <101904585+andherre@users.noreply.github.com>
2022-07-04 17:51:32 -0500

    Update ingest_data.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -1,19 +1,19 @@
 """
 Módulo de ingestión de datos.
 -------------------------------------------------------------------------------
+En este módulo se descargan los datos externos y se almacenan en la carpeta landing del data lake
+
+>>> ingest_data()
 
 """
 
-
 def ingest_data():
     """Ingeste los datos externos a la capa landing del data lake.
-
     Del repositorio jdvelasq/datalabs/precio_bolsa_nacional/xls/ descarge los
     archivos de precios de bolsa nacional en formato xls a la capa landing. La
     descarga debe realizarse usando únicamente funciones de Python.
-    raise NotImplementedError("Implementar esta función")
     """
-   
+    #raise NotImplementedError("Implementar esta función")
 
     import os
     import urllib.request
