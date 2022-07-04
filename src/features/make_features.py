6281007693bf4513be9a626f35ac22d906b35122
andherre <101904585+andherre@users.noreply.github.com>
2022-07-04 18:44:53 -0500

    Update make_features.py

--- src/features/make_features.py
+++ src/features/make_features.py
@@ -1,28 +1,28 @@
+"""
+Módulo de preparación de datos para pronóstico.
+-------------------------------------------------------------------------------
+En este módulo se toma el archivo precios-diarios.csv y se obtiene una columna de clasificación según el día,
+esto con la finalidad de tener datos separados (días anteriores y días posteriores) para posteriormente
+hacer modelación y pronosticar.
+
+>>> make_features()
+
+"""
+
 def make_features():
     """Prepara datos para pronóstico.
-
+    
     Cree el archivo data_lake/business/features/precios-diarios.csv. Este
     archivo contiene la información para pronosticar los precios diarios de la
     electricidad con base en los precios de los días pasados. Las columnas
     correspoden a las variables explicativas del modelo, y debe incluir,
     adicionalmente, la fecha del precio que se desea pronosticar y el precio
     que se desea pronosticar (variable dependiente).
-
     En la carpeta notebooks/ cree los notebooks de jupyter necesarios para
     analizar y determinar las variables explicativas del modelo.
 
-    if __name__ == "__main__":
-        import doctest
-    make_features()
-    doctest.testmod()
-
-    if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
-    raise NotImplementedError("Implementar esta función")
     """
-    
+    #raise NotImplementedError("Implementar esta función")
     import pandas as pd
 
     precios_diarios = pd.read_csv('data_lake/business/precios-diarios.csv')
@@ -36,6 +36,4 @@ if __name__ == "__main__":
     import doctest
     make_features()
     doctest.testmod()
-
-
 
