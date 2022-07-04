c4e042a220fb9b1e5ca94a40f8a50ceccdcbaac2
andherre <101904585+andherre@users.noreply.github.com>
2022-07-04 10:28:30 -0500

    Update compute_monthly_prices.py

--- src/data/compute_monthly_prices.py
+++ src/data/compute_monthly_prices.py
@@ -9,10 +9,24 @@ def compute_monthly_prices():
 
     * precio: precio promedio mensual de la electricidad en la bolsa nacional
 
-
+    raise NotImplementedError("Implementar esta función")
 
     """
-    raise NotImplementedError("Implementar esta función")
+   
+    import pandas as pd
+
+    price_hourly = pd.read_csv(f'data_lake/cleansed/precios-horarios.csv')
+    price_hourly['fecha'] = pd.to_datetime(price_hourly['fecha'])
+    price_hourly.set_index('fecha', inplace = True)
+    price_month = price_hourly.resample('M').mean()
+    price_month_nohour = price_month.drop(['hora'], axis=1)
+    price_month_nohour.to_csv(f'data_lake/business/precios-mensuales.csv', header = True, index = True)
+
+
+if __name__ == "__main__":
+    import doctest
+    compute_monthly_prices()
+    doctest.testmod()
 
 
 if __name__ == "__main__":
