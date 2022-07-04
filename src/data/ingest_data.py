6d3b3b7e1672c8af545e6c08c6c8e5010ef6b832
andherre <101904585+andherre@users.noreply.github.com>
2022-07-04 10:28:34 -0500

    Update ingest_data.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -11,12 +11,34 @@ def ingest_data():
     Del repositorio jdvelasq/datalabs/precio_bolsa_nacional/xls/ descarge los
     archivos de precios de bolsa nacional en formato xls a la capa landing. La
     descarga debe realizarse usando únicamente funciones de Python.
-
-    """
     raise NotImplementedError("Implementar esta función")
+    """
+   
 
+    import os
+    import urllib.request
+
+    initial_year = 1995
+    final_year = 2021
+    range_year = list(range(initial_year, final_year + 1, 1))
+
+    for anio in range_year:
+        try:
+            archivo = open(f'data_lake/landing/{anio}.xlsx', 'wb')
+            ruta = urllib.request.urlopen(f'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{anio}.xlsx?raw=true')
+            archivo.write(ruta.read())
+            archivo.close()
+        except:
+            archivo.close()
+            os.remove(f'data_lake/landing/{anio}.xlsx')
+
+            archivo = open(f'data_lake/landing/{anio}.xls', 'wb')
+            ruta = urllib.request.urlopen(f'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{anio}.xls?raw=true')
+            archivo.write(ruta.read())
+            archivo.close()
 
 if __name__ == "__main__":
     import doctest
-
+    ingest_data()
     doctest.testmod()
+
