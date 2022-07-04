bd425475ceca1d087e26019a7bc1eeb7bb45af61
andherre <101904585+andherre@users.noreply.github.com>
2022-07-04 18:45:01 -0500

    Update make_daily_prices_plot.py

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -1,15 +1,17 @@
+"""
+
+Módulo de gráfica precios promedio diarios.
+-------------------------------------------------------------------------------
+En este módulo se carga el archivo precios-diarios.csv,
+y se efectúa una gráfica para observar el comportamiento general del valor
+de la electridad en la bolsa de valores.
+
+>>> make_daily_prices_plot()
+
+"""
+
 def make_daily_prices_plot():
     """Crea un grafico de lines que representa los precios promedios diarios.
-
-    Usando el archivo data_lake/business/precios-diarios.csv, crea un grafico de
-    lines que representa los precios promedios diarios.
-
-    El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
-    raise NotImplementedError("Implementar esta función")
-    """
-   
-    def make_daily_prices_plot():
-        """Crea un grafico de lines que representa los precios promedios diarios.
     Usando el archivo data_lake/business/precios-diarios.csv, crea un grafico de
     lines que representa los precios promedios diarios.
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
