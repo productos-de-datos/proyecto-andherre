7b8c3e3aaad7997064a16494cb382d5fae6f0748
andherre <101904585+andherre@users.noreply.github.com>
2022-07-04 10:28:32 -0500

    Update create_data_lake.py

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -18,11 +18,21 @@ def create_data_lake():
               |___ forecasts/
 
     ```
-
-
-    """
     raise NotImplementedError("Implementar esta función")
 
+    """
+
+    import os
+
+    os.mkdir('data_lake')
+    os.mkdir('data_lake/landing')
+    os.mkdir('data_lake/raw')
+    os.mkdir('data_lake/cleansed')
+    os.mkdir('data_lake/business')
+    os.mkdir('data_lake/business/reports')
+    os.mkdir('data_lake/business/reports/figures')
+    os.mkdir('data_lake/business/features')
+    os.mkdir('data_lake/business/forecasts')
 
 if __name__ == "__main__":
     import doctest
