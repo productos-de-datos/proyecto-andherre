613a56b88fb11970f0b21794d4bed059effbfd29
andherre <101904585+andherre@users.noreply.github.com>
2022-07-05 11:16:52 -0500

    Update transform_data.py

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -1,52 +1,60 @@
 
-"""Transforme los archivos xls a csv.
+"""
+Módulo de transformación de datos.
+-------------------------------------------------------------------------------
+En este módulo se toman los archivos .xlsx de la carpeta landing, se transforman a archivos delimitados por coma
+y se almacenan en la carpeta raw del data lake, con la finalidad de tener datos de entrada consistentes entre los
+diferentes años (mismo número de columnas, mismo formato de fecha)
+>>> transform_data()
+"""
 
+
+def transform_data():
+    """Transforme los archivos xls a csv.
     Transforme los archivos data_lake/landing/*.xls a data_lake/raw/*.csv. Hay
     un archivo CSV por cada archivo XLS en la capa landing. Cada archivo CSV
     tiene como columnas la fecha en formato YYYY-MM-DD y las horas H00, ...,
     H23.
-    raise NotImplementedError("Implementar esta función")
-
-    """
-import pandas as pd
-import os
-
-
-def transform_data():
-    """
-    Transforma los archivos xls a csv.
     """
     #raise NotImplementedError("Implementar esta función")
-    #cwd=os.getcwd()
+    import subprocess
+    import os
 
-    path_landing = 'data_lake/landing/'
-    path_raw = 'data_lake/raw/'
-    list_files = os.listdir(path_landing)
+    subprocess.call(["pip", "install", "openpyxl"])
+    subprocess.call(["pip", "install", "xlrd"])
 
-    for file in list_files:
+    import pandas as pd
+
+    contenido_landing = os.listdir('data_lake/landing')
+    anio_inicial = 1995
+    anio_final = 2021
+
+    for elemento in range(anio_inicial, anio_final + 1, 1):
         
-        try :
-            df_energia = pd.read_excel(path_landing + file,engine='openpyxl')
-      
-        except :
-            df_energia = pd.read_excel(path_landing + file )
-        
-        if  df_energia.columns[0]!='Fecha':
-            skip=df_energia.iloc[:,0][df_energia.iloc[:,0]=='Fecha'].index.tolist()[0]
-            df_energia=df_energia.iloc[skip:,:]
-            new_header = df_energia.iloc[0]
-            df_energia = df_energia[1:] 
-            df_energia.columns = new_header
-        
-        df_energia=df_energia.iloc[:,:25]
-        df_energia=df_energia.dropna()
-        df_energia = df_energia.melt(id_vars=['Fecha'], value_name='Precio', var_name='Hora')
-        df_energia['Hora']=df_energia['Hora'].astype(int)
-        df_energia['Fecha']=pd.to_datetime(df_energia['Fecha'], format='%Y-%m-%d')
-        df_energia=df_energia.sort_values(['Fecha','Hora']).fillna(method='ffill')
-        df_energia=df_energia.sort_values(['Fecha','Hora']).fillna(method='bfill')
-        df_energia=df_energia.drop_duplicates()
-        df_energia.to_csv(path_raw+file.split('.')[0]+'.csv',index=False)
+        archivo = pd.read_excel(f'data_lake/landing/{contenido_landing[elemento-anio_inicial]}', skiprows=3)
+
+        if archivo.columns[0] == 'Fecha':
+            if archivo.shape[1] > 25:
+                archivo = archivo.iloc[:, 0:25]         
+        else:
+            archivo = pd.read_excel(f'data_lake/landing/{contenido_landing[elemento-1995]}', skiprows=2)
+            if archivo.columns[0] == 'Fecha':
+                if archivo.shape[1] > 25:
+                    archivo = archivo.iloc[:, 0:25]
+            else:
+                archivo = pd.read_excel(f'data_lake/landing/{contenido_landing[elemento-1995]}', skiprows=1)
+                if archivo.columns[0] == 'Fecha':
+                    if archivo.shape[1] > 25:
+                        archivo = archivo.iloc[:, 0:25]
+                else:
+                    archivo = pd.read_excel(f'data_lake/landing/{contenido_landing[elemento-1995]}', skiprows=0)
+                    if archivo.columns[0] == 'Fecha':
+                        if archivo.shape[1] > 25:
+                            archivo = archivo.iloc[:, 0:25]                
+
+        archivo['Fecha'] = pd.to_datetime(archivo['Fecha'])
+        archivo.columns = ['Fecha', 'H00', 'H01', 'H02', 'H03', 'H04', 'H05', 'H06', 'H07', 'H08', 'H09', 'H10', 'H11', 'H12', 'H13', 'H14', 'H15', 'H16', 'H17', 'H18', 'H19', 'H20', 'H21', 'H22', 'H23']
+        archivo.to_csv(f'data_lake/raw/{elemento}.csv', header = True, index = False)
 
 if __name__ == "__main__":
     import doctest
