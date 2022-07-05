b15745e1f6ea800cdd954c77e40393507c1d7e87
andherre <101904585+andherre@users.noreply.github.com>
2022-07-04 21:43:58 -0500

    Update make_forecasts.py

--- src/models/make_forecasts.py
+++ src/models/make_forecasts.py
@@ -17,9 +17,11 @@ def make_forecasts():
     """Construya los pronosticos con el modelo entrenado final.
     Cree el archivo data_lake/business/forecasts/precios-diarios.csv. Este
     archivo contiene tres columnas:
+
     * La fecha.
     * El precio promedio real de la electricidad.
     * El pronóstico del precio promedio real.
+    
     """
     #raise NotImplementedError("Implementar esta función")
 
