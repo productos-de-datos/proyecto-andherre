eb4539f24cde8189b19b3a23bd7c3b9fc51ca5b4
andherre <101904585+andherre@users.noreply.github.com>
2022-07-05 10:15:32 -0500

    Update make_features.py

--- src/features/make_features.py
+++ src/features/make_features.py
@@ -9,31 +9,24 @@ hacer modelación y pronosticar.
 
 """
 
+import pandas as pd
+
+
 def make_features():
     """Prepara datos para pronóstico.
-    
-    Cree el archivo data_lake/business/features/precios-diarios.csv. Este
-    archivo contiene la información para pronosticar los precios diarios de la
-    electricidad con base en los precios de los días pasados. Las columnas
-    correspoden a las variables explicativas del modelo, y debe incluir,
-    adicionalmente, la fecha del precio que se desea pronosticar y el precio
-    que se desea pronosticar (variable dependiente).
-    En la carpeta notebooks/ cree los notebooks de jupyter necesarios para
-    analizar y determinar las variables explicativas del modelo.
-
+    Debido a que es una serie de tiempo no es necesario crear mas caracteristicas
     """
-    #raise NotImplementedError("Implementar esta función")
-    import pandas as pd
+    try :
+        dfp = pd.read_csv("data_lake/business/precios-diarios.csv")
 
-    precios_diarios = pd.read_csv('data_lake/business/precios-diarios.csv')
-    precios_diarios['fecha'] = pd.to_datetime(precios_diarios['fecha'])
-    precios_diarios['dia_mes'] = precios_diarios['fecha'].dt.day
-    precios_diarios['dia_mes_binario'] = (precios_diarios['dia_mes']>20).astype(int)
-    precios_diarios.to_csv('data_lake/business/features/precios_diarios.csv', index = False)
+        dfp.to_csv('data_lake/business/features/precios-diarios.csv',index=False)
 
+        return dfp
+
+    except : # pylint: disable=W0702
+        return None
 
 if __name__ == "__main__":
     import doctest
     make_features()
     doctest.testmod()
-
