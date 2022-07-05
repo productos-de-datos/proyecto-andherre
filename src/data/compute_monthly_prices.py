e1df4ce1cfb2463dcc007d6bd8e5af7b132d457a
andherre <101904585+andherre@users.noreply.github.com>
2022-07-05 08:58:35 -0500

    Update compute_monthly_prices.py

--- src/data/compute_monthly_prices.py
+++ src/data/compute_monthly_prices.py
@@ -9,28 +9,26 @@ promedio que tuvo la electricidad en la bolsa nacional.
 
 """
 
+import os
+import pandas as pd
+
 def compute_monthly_prices():
     """Compute los precios promedios mensuales.
-    
     Usando el archivo data_lake/cleansed/precios-horarios.csv, compute el prcio
     promedio mensual. Las
     columnas del archivo data_lake/business/precios-mensuales.csv son:
-
     * fecha: fecha en formato YYYY-MM-DD
-
     * precio: precio promedio mensual de la electricidad en la bolsa nacional
-
     """
-    #raise NotImplementedError("Implementar esta función")
-    import pandas as pd
-
-    precios_horarios = pd.read_csv(f'data_lake/cleansed/precios-horarios.csv')
-    precios_horarios['fecha'] = pd.to_datetime(precios_horarios['fecha'])
-    precios_horarios.set_index('fecha', inplace = True)
-    precios_mensuales = precios_horarios.resample('M').mean()
-    precios_mensuales_sin_hora = precios_mensuales.drop(['hora'], axis=1)
-    precios_mensuales_sin_hora.to_csv(f'data_lake/business/precios-mensuales.csv', header = True, index = True)
-
+    # raise NotImplementedError("Implementar esta función")
+    cwd=os.getcwd()
+    path=os.path.join(cwd, 'data_lake/cleansed/precios-horarios.csv') 
+    df=pd.read_csv(path)
+    df['Precio']=df['Precio'].astype('float')
+    df['Fecha']=pd.to_datetime(df['Fecha'], format='%Y-%m-%d')
+    df=df[['Fecha','Precio']].groupby(pd.Grouper(key='Fecha', freq='1M')).mean().reset_index()
+    df['Fecha']=df['Fecha'].apply(lambda dt: dt.replace(day=1))
+    df.to_csv(os.path.join(cwd, 'data_lake/business/precios-mensuales.csv') , index=False )
 
 if __name__ == "__main__":
     import doctest
